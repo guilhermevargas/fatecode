8d0b4b656af193efcc203811eb07fb2b20189f60
Guilherme Alves Vargas <guilherme.alves.vargas@outlook.com>
2014-08-31 15:01:33 -0300

    Implementação da classe MovimentacaoFinanceira - tarefa0702

---
+++ fatecode/src/com/wordpress/fatecode/tarefa0702/MovimentacaoFinanceira.java
@@ -0,0 +1,24 @@
+package com.wordpress.fatecode.tarefa0702;
+
+import java.util.Date;
+
+public class MovimentacaoFinanceira {
+	private Date timeStamp;
+	private String tipo;
+	private double valor;
+	
+	public MovimentacaoFinanceira(Date timeStamp, String tipo, double valor) {
+		this.timeStamp = timeStamp;
+		this.tipo = tipo;
+		this.valor = valor;
+	}
+	
+	public String getMovimentacao() {
+		String dadosMovimentacao = "Data: " + timeStamp.toString();
+		dadosMovimentacao += " Tipo: " + tipo;
+		dadosMovimentacao += " Valor: " + valor;
+		
+		return dadosMovimentacao;
+	}
+ 
+}
