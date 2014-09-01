a4137cd41b02c031d5de92cb7b3b0143da0eb845
Guilherme Alves Vargas <guilherme.alves.vargas@outlook.com>
2014-09-01 14:46:50 -0300

    refatoração em metodos

--- fatecode/src/com/wordpress/fatecode/tarefa0701/ContaBancaria.java
+++ fatecode/src/com/wordpress/fatecode/tarefa0701/ContaBancaria.java
@@ -1,45 +1,49 @@
 package com.wordpress.fatecode.tarefa0701;
 
+import java.util.ArrayList;
+
 public class ContaBancaria {
 	private int numero;
 	private String nome;
 	private String cpf;
 	private double saldo;
 	private double limite;
-	private String[] extrato;
-	
+	private ArrayList<String> extrato;
+
 	public ContaBancaria(int numero, String nome, String cpf, double saldo) {
 		this.numero = numero;
 		this.nome = nome;
 		this.cpf = cpf;
 		this.saldo = saldo;
+		extrato = new ArrayList<>();
 	}
-	
+
 	public void sacar(double valor, String dadosMovimentacao) {
 		if (valor <= this.saldo + this.limite) {
 			this.saldo -= valor;
-			extrato[extrato.length] = dadosMovimentacao;
-		} else { 
+			extrato.add(dadosMovimentacao);
+		} else {
 			throw new IllegalArgumentException("Saldo insuficiente.");
 		}
 	}
-	
+
 	public void depositar(double valor, String dadosMovimentacao) {
 		if (valor > 0) {
-			this.saldo =+ valor;
-			extrato[extrato.length] = dadosMovimentacao;
-		} else { 
-			throw new IllegalArgumentException("O valor do deposito deve ser positivo.");
+			this.saldo = +valor;
+			extrato.add(dadosMovimentacao);
+		} else {
+			throw new IllegalArgumentException(
+					"O valor do deposito deve ser positivo.");
 		}
 	}
-	
+
 	public String getExtrato() {
 		String dados = "";
-		
+
 		for (String movimento : extrato) {
 			dados += movimento + "\n";
-		} 
-		
+		}
+
 		return dados;
 	}
 
@@ -74,5 +78,14 @@ public class ContaBancaria {
 	public void setLimite(double limite) {
 		this.limite = limite;
 	}
-	
+
+	@Override
+	public boolean equals(Object obj) {
+		if ((obj instanceof ContaBancaria)
+				&& this.numero == ((ContaBancaria) obj).numero)
+			return true;
+		else
+			return false;
+	}
+
 }
--- fatecode/src/com/wordpress/fatecode/tarefa0704/Main.java
+++ fatecode/src/com/wordpress/fatecode/tarefa0704/Main.java
@@ -1,28 +1,28 @@
 package com.wordpress.fatecode.tarefa0704;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.Scanner;
 
 import com.wordpress.fatecode.tarefa0701.ContaBancaria;
 import com.wordpress.fatecode.tarefa0702.MovimentacaoFinanceira;
+import com.wordpress.fatecode.tarefa0702.MovimentacaoSaque;
 
 public class Main {
+	public static final String OPCOES_MENU = "1: Criar uma nova conta \n"
+			+ "2: Definir o limite de uma conta \n"
+			+ "3: Realizar uma retirada de uma conta \n"
+			+ "4: Realizar um depósito em uma conta \n"
+			+ "5: Visualizar o saldo e limite de uma conta \n"
+			+ "6: Visualizar o extrato de uma conta \n"
+			+ "7: excluir uma conta \n" + "Entre com a opção desejada: ";
 
-	public static int exibirMenu() {
-		String opcoesMenu = "1: Criar uma nova conta \n";
-		opcoesMenu += "2: Definir o limite de uma conta \n";
-		opcoesMenu += "3: Realizar uma retirada de uma conta \n";
-		opcoesMenu += "4: Realizar um depósito em uma conta \n";
-		opcoesMenu += "5: Visualizar o saldo e limite de uma conta \n";
-		opcoesMenu += "6: Visualizar o extrato de uma conta \n";
-		opcoesMenu += "7: excluir uma conta";
-		opcoesMenu += "Entre com a opção desejada: ";
-		System.out.println(opcoesMenu);
+	ArrayList<ContaBancaria> listaContas = new ArrayList<>();
 
-		return criarScanner().nextInt();
-	}
-	
 	/**
 	 * Cria Scanner
+	 * 
 	 * @return scanner para obter entrada de dados
 	 */
 	public static Scanner criarScanner() {
@@ -31,28 +31,111 @@ public class Main {
 	}
 
 	/**
-	 * Efetua a trasacao a partir do tipo de movimentação 
+	 * Efetua a trasacao a partir do tipo de movimentação
+	 * 
 	 * @param movimentacao
 	 * @param conta
 	 */
-	public static void efetuarTransacao(MovimentacaoFinanceira movimentacao, ContaBancaria conta) {
+	public void efetuarTransacao(MovimentacaoFinanceira movimentacao,
+			ContaBancaria conta) {
 		movimentacao.efetuarTransacao(conta);
 	}
-	
+
 	/**
 	 * 
 	 * @param contas
 	 * @param conta
 	 */
-	public void excluir(ContaBancaria[] contas, ContaBancaria conta) {
-		for (ContaBancaria contaAtual : contas) {
-			if (contaAtual.getNumero() == conta.getNumero()) {
-				contaAtual = null;
+	public void excluir(int numeroConta) {
+
+	}
+
+	/**
+	 * Salva conta bancaria
+	 * 
+	 * @param contaBancaria
+	 */
+	public void salvar(ContaBancaria contaBancaria) {
+		listaContas.add(contaBancaria);
+	}
+
+	/**
+	 * Método abre campos para entrada de dados
+	 * 
+	 * @return retorna uma conta bancaria ainda não salva
+	 */
+	public static ContaBancaria criarContaBancaria() {
+		Scanner input = criarScanner();
+
+		System.out.println("Entre com o número: ");
+		int numero = input.nextInt();
+
+		System.out.println("Entre com o nome: ");
+		String nome = input.next();
+
+		System.out.println("Entre com cpf: ");
+		String cpf = input.next();
+
+		System.out.println("Entre com o saldo: ");
+		double saldo = input.nextDouble();
+
+		return new ContaBancaria(numero, nome, cpf, saldo);
+	}
+
+	/**
+	 * Buscar conta bancaria a partir do numero da conta
+	 * 
+	 * @param numero
+	 * @return conta bancaria encontrada, ou retorna null caso não encontre
+	 */
+	public ContaBancaria buscar(int numero) {
+		Iterator<ContaBancaria> i = listaContas.iterator();
+
+		while (i.hasNext()) {
+			ContaBancaria contaBancaria = i.next();
+			
+			if (contaBancaria.getNumero() == numero)
+				return contaBancaria;
+		}
+
+		return null;
+	}
+
+	public static void main(String[] args) {
+		Main principal = new Main();
+		Scanner input = criarScanner();
+
+		
+
+		ContaBancaria contaBancaria = null;
+		
+		
+		while (true) {
+			System.out.println(OPCOES_MENU);
+			int opcao = input.nextInt();
+			
+			switch (opcao) {
+			case 1:
+				contaBancaria = criarContaBancaria();
+				principal.salvar(contaBancaria);
+				break;
+
+			case 2:
+				System.out.println("Entre com o numero da conta");
+				int numero = input.nextInt();
+				contaBancaria = principal.buscar(numero);
+
+				System.out.println("Entre com o valor do saque");
+				double valor = input.nextDouble();
+				MovimentacaoFinanceira movimentacao = new MovimentacaoSaque(
+						new Date(System.currentTimeMillis()), "Saque", valor);
+				
+				principal.efetuarTransacao(movimentacao, contaBancaria);
+
+				System.out.println("Saque efetuado!");
+
+			case 3:
 			}
 		}
-	}
-	
-	public static void main(String[] args) {
-		
 	}
 }
